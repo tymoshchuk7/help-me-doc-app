18d9d8fc6a4d100b05f6b5c536444690a0d245e7
otymoshchuk <otymoshchuk@wemyit.com>
2023-04-02 16:16:34 +0300

    chore: update login and sign up forms

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,40 @@
 import React, { ReactElement, useState } from 'react';
-import { Input, Row, Col, Button } from 'antd';
+import { Input, Row, Col, Button, Form } from 'antd';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts';
 
 const Login = (): ReactElement => {
   const { onLogin } = useAuth();
+  const [form] = Form.useForm();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [, setError] = useState<null | unknown>(null);
 
   const onSubmit = async () => {
-    await onLogin({ email, password });
+    try {
+      await onLogin({ email, password });
+    } catch (e) {
+      setError(e);
+    }
   };
 
   return (
     <div>
       <Row gutter={[16, 16]} justify="center" align="middle" className="min-page-height">
         <Col span={9}>
-          <Input placeholder="email" value={email} onChange={({ target }) => setEmail(target.value)} />
-          <Input
-            placeholder="password"
-            type="password" className="mt-2"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <div className="mt-2">
-            Don't have account? <Link to="/signup">Signup</Link>
-          </div>
-          <Button className="mt-2" onClick={onSubmit}>Login</Button>
+          <Form onFinish={onSubmit} form={form}>
+            <Input placeholder="email" value={email} onChange={({ target }) => setEmail(target.value)} />
+            <Input
+              placeholder="password"
+              type="password" className="mt-2"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <div className="mt-2">
+              Don't have an account? <Link to="/signup">Signup</Link>
+            </div>
+            <Button className="mt-2" htmlType="submit">Login</Button>
+          </Form>
         </Col>
       </Row>
     </div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,33 +1,44 @@
 import React, { ReactElement, useState } from 'react';
-import { Input, Row, Col, Button } from 'antd';
+import { Input, Row, Col, Button, Form } from 'antd';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts';
 
 //TODO add validation
 const SignUp = (): ReactElement => {
   const { onSignUp } = useAuth();
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [, setError] = useState<null | unknown>(null);
 
   const onSubmit = async () => {
-    await onSignUp({ email, password });
+    try {
+      await onSignUp({ email, password });
+      navigate('/login');
+    } catch (e) {
+      setError(e);
+    }
   };
 
   return (
     <div>
       <Row gutter={[16, 16]} justify="center" align="middle" className="min-page-height">
         <Col span={9}>
-          <Input placeholder="email" value={email} onChange={({ target }) => setEmail(target.value)} />
-          <Input
-            placeholder="password"
-            type="password" className="mt-2"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <div className="mt-2">
-            Already have account? <Link to="/login">Login</Link>
-          </div>
-          <Button className="mt-2" onClick={onSubmit}>Sign Up</Button>
+          <Form onFinish={onSubmit} form={form}>
+            <Input placeholder="email" value={email} onChange={({ target }) => setEmail(target.value)} />
+            <Input
+              placeholder="password"
+              type="password" className="mt-2"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <div className="mt-2">
+              Already have an account? <Link to="/login">Login</Link>
+            </div>
+            <Button htmlType="submit" className="mt-2">Sign Up</Button>
+          </Form>
         </Col>
       </Row>
     </div>

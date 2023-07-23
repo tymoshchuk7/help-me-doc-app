9720f6a086b29ef912f8cfd3bfbd1c2897b68d15
tymoshchuk7 <tymoshchukalex7@gmail.com>
2023-07-23 23:25:20 +0300

    feat: implement invite participant form

--- src/components/InviteParticipant.tsx
+++ src/components/InviteParticipant.tsx
@@ -7,12 +7,12 @@ import { AppDispatch } from '../store';
 const { Option } = Select;
 
 const layout = {
-  labelCol: { span: 8 },
+  labelCol: { span: 3 },
   wrapperCol: { span: 16 },
 };
 
 const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
+  wrapperCol: { offset: 2, span: 16 },
 };
 
 const App: React.FC = () => {
@@ -28,36 +28,40 @@ const App: React.FC = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      form={form}
-      name="control-hooks"
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-    >
-      <Form.Item name="email" label="Email" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="role" label="Role" rules={[{ required: true }]}>
-        <Select
-          placeholder="Select a option and change input text above"
-          allowClear
-        >
-          <Option value="chief">Chief</Option>
-          <Option value="patient">Patient</Option>
-          <Option value="doctor">Doctor</Option>
-          <Option value="admin">Admin</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
-        </Button>
-      </Form.Item>
-    </Form>
+    <>
+      <h3>Invite a participant</h3>
+      <Form
+        {...layout}
+        form={form}
+        name="control-hooks"
+        onFinish={onFinish}
+        style={{ maxWidth: 600 }}
+        className="mt-2"
+      >
+        <Form.Item name="email" label="Email" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="role" label="Role" rules={[{ required: true }]}>
+          <Select
+            placeholder="Select a option and change input text above"
+            allowClear
+          >
+            <Option value="chief">Chief</Option>
+            <Option value="patient">Patient</Option>
+            <Option value="doctor">Doctor</Option>
+            <Option value="admin">Admin</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button htmlType="button" onClick={onReset} className="ml-2">
+            Reset
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as PageLayout } from './Layout';
 export { default as Loader } from './Loader';
+export { default as InviteParticipant } from './InviteParticipant';

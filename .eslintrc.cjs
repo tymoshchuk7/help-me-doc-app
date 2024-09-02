934fdcae5a497244974af68dc8956ff56846a8d1
tymoshchuk7 <tymoshchukalex7@gmail.com>
2024-09-02 23:53:16 +0300

    chore: set up eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,18 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.eslint.json',
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],

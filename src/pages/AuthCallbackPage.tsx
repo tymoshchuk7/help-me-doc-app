c559d162c76a05fc1fc4bf614b9f41ae9ad4dda4
tymoshchuk7 <tymoshchukalex7@gmail.com>
2024-09-14 17:45:11 +0300

    feat: implement signup and auth callback pages

---
+++ src/pages/AuthCallbackPage.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement, useEffect } from 'react';
+import qs from 'qs';
+import { useNavigate, Link } from 'react-router-dom';
+import { Space, Alert } from 'antd';
+import { useAuth } from '../contexts';
+
+interface AuthCallbackData {
+  id_token: string,
+  access_token: string,
+  error: string,
+  errorDescription: string,
+}
+
+const AuthCallback = (): ReactElement => {
+  const {
+    id_token: idToken, error: authError,
+  } = qs.parse(window.location.hash.slice(1)) as unknown as AuthCallbackData;
+  const navigate = useNavigate();
+  const { updateToken } = useAuth();
+
+  useEffect(() => {
+    updateToken(idToken);
+    navigate('/');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (authError) {
+    return (
+      <Space>
+        <Alert
+          message="Error"
+          description={authError}
+          type="error"
+        />
+        <Link to="/login">Back to login page</Link>
+      </Space>
+    );
+  }
+
+  return <></>;
+};
+
+export default AuthCallback;
---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,71 @@
+import React, { ReactElement, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Auth0Error } from 'auth0-js';
+import { Button, Form, FormProps } from 'antd';
+import { IUser } from '../types';
+import { AuthPageLayout, Input } from '../components';
+import { userValidator } from '../validators';
+import { useAuth } from '../contexts';
+
+const SignUpPage = (): ReactElement => {
+  const { onSignUp } = useAuth();
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [error, setError] = useState<null | Auth0Error>(null);
+
+  const onSubmit: FormProps<IUser>['onFinish'] = async (values) => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { email, password, first_name, last_name } = values;
+    try {
+      await onSignUp({
+        email,
+        password,
+        first_name,
+        last_name,
+      });
+      navigate('/login');
+    } catch (e) {
+      setError(e as Auth0Error);
+    }
+  };
+
+  return (
+    <AuthPageLayout errorMessage={error?.description || null}>
+      <Form onFinish={onSubmit} form={form} layout="vertical">
+        <Input
+          name="email"
+          label="Email"
+          placeholder="email"
+          rules={userValidator.email}
+        />
+        <Input
+          name="first_name"
+          label="First name"
+          placeholder="first name"
+          rules={userValidator.first_name}
+        />
+        <Input
+          name="last_name"
+          label="Last name"
+          placeholder="last name"
+          rules={userValidator.last_name}
+        />
+        <Input
+          name="password"
+          label="Password"
+          placeholder="password"
+          rules={userValidator.password}
+          hideValue
+        />
+        <div className="mt-2">
+          Already have an account?
+          {' '}
+          <Link to="/login">Login</Link>
+        </div>
+        <Button style={{ marginTop: 20 }} htmlType="submit">Sign Up</Button>
+      </Form>
+    </AuthPageLayout>
+  );
+};
+
+export default SignUpPage;

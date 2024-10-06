88a31000f4cc8c441edb588e2ec5da00c5637497
tymoshchuk7 <tymoshchukalex7@gmail.com>
2024-10-07 00:19:50 +0300

    fix: temporary ignored error of eslint module at vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// TODO research an other options to resolve TS error below
+// @ts-ignore
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({

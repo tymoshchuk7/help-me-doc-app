f7dc5f349a34a3f63e1b1a9b488b4c4d3375956f
tymoshchuk7 <tymoshchukalex7@gmail.com>
2024-12-01 18:04:32 +0200

    chore: update layout-content styles

--- src/components/app/Layout.tsx
+++ src/components/app/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, useState } from 'react';
-import { Layout } from 'antd';
+import { Layout, theme } from 'antd';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -10,6 +10,7 @@ import NewDiseaseModal from './NewDiseaseModal';
 const { Content } = Layout;
 
 const AppLayout = (): ReactElement => {
+  const { token: { colorBgContainer, borderRadiusLG } } = theme.useToken();
   const [modalVisibility, setModalVisibility] = useState<null | ModalType>(null);
 
   const hideModal = () => setModalVisibility(null);
@@ -28,6 +29,8 @@ const AppLayout = (): ReactElement => {
               padding: 24,
               margin: 0,
               minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
             }}
           >
             <Outlet />
